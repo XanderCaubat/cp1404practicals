efc2f100be183b7d9a13f4bdc6506dd8bef51104
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-09-25 18:24:59 +1000

    Remove # pseudocode from score menu

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -7,22 +7,6 @@ uses a main and other functions
 """
 
 
-# display menu
-# get choice
-# while choice != <quit option>
-#     if choice == <first option>
-#         <do first task>
-#     else if choice == <second option>
-#         <do second task>
-#     ...
-#     else if choice == <n-th option>
-#         <do n-th task>
-#     else
-#         display invalid input error message
-#     display menu
-#     get choice
-# <do final thing, if needed>
-
 def main():
     score = 0
     print_menu()
