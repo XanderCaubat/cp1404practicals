704ba40994b1388e782c8448d4792a72fb497d38
jc785304 <xanderdino.caubat@my.jcu.edu.au>
2022-09-29 16:28:14 +1000

    Modify randoms program to print various random modules

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -3,3 +3,9 @@ Xander Dino Caubat
 CP1404/CP5632 - Practical
 This program prints random numbers using random module.
 """
+
+import random
+
+print((random.randint(5, 20)))  # The smallest number seen was 5 and the largest number seen was 18.
+print(random.randrange(3, 10, 2))  # The smallest number seen was 3 and largest was 9. No, line 2 could not produce 4.
+print(random.uniform(2.5, 5.5))     # The smallest number seen was 2.5436002992019238 and largest was 5.158785122603284.
--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -1,10 +1,7 @@
 """
 Xander Dino Caubat
 CP1404/CP5632 - Practical
-Various examples of using Python string formatting.
-(We prefer f-strings in this subject.)
-Want to read more about it?
-https://docs.python.org/3/library/string.html#formatstrings
+This program prints various string formatting.
 """
 
 name = "Gibson L-5 CES"
