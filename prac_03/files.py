6366273d4a137485c728e394e9b1240de08083e8
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-01 23:19:36 +1000

    Modify file program to read numbers text file

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,15 +1,22 @@
 """
 CP1404/CP5632 - Practical 3
 Files
-This program ask the user for their name and then it writes it into a file.
+This program ask the user for their name, and then it writes it into a file.
 """
 
-name = input("Name: ").capitalize()
-with open("name.txt", "w") as out_file:
-    print(name, file=out_file)
-    out_file.close()
+# name = input("Name: ").capitalize()
+# with open("name.txt", "w") as out_file:
+#     print(name, file=out_file)
+#     out_file.close()
+#
+# with open("name.txt", "r") as in_file:
+#     name = in_file.readline()
+#     print(f"Your name is {name}")
+#     in_file.close()
 
-with open("name.txt", "r") as in_file:
-    name = in_file.readline()
-    print(f"Your name is {name}")
+
+with open("numbers.txt", "r") as in_file:
+    numbers = in_file.readlines()
+    total = int(numbers[0]) + int(numbers[1])
+    print(total)
     in_file.close()
