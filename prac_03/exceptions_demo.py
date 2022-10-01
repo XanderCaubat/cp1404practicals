ae3393a75dcb31d64e554f2f5ddeeb85904fb656
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-01 21:21:25 +1000

    Modify exceptions demo program to use boolean in while loop

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -4,13 +4,15 @@ Exceptions Demo
 This program takes user's number inputs and calculates into fraction.
 """
 
-try:
-    numerator = int(input("Enter the numerator: "))
-    denominator = int(input("Enter the denominator: "))
-    fraction = numerator / denominator
-    print(fraction)
-except ValueError:
-    print("Numerator and denominator must be valid numbers!")  # this occurs when the numbers entered are not integer
-except ZeroDivisionError:
-    print("Cannot divide by zero!")  # this occurs when the numerator is divided by zero
+is_valid_input = False
+while not is_valid_input:
+    try:
+        numerator = int(input("Enter the numerator: "))
+        denominator = int(input("Enter the denominator: "))
+        fraction = numerator / denominator
+        print(fraction)
+    except ValueError:
+        print("Numerator and denominator must be valid numbers!")  # this occurs when the numbers entered are not integer
+    except ZeroDivisionError:
+        print("Cannot divide by zero!")  # this occurs when the numerator is divided by zero
 print("Finished.")
