97639e7fe7a6f7b3c6e52c4258f63831c62339cb
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-01 22:33:05 +1000

    Modify file program to read and write name text file

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,8 +4,12 @@ Files
 This program ask the user for their name and then it writes it into a file.
 """
 
-name = input("Name: ")
+name = input("Name: ").capitalize()
 with open("name.txt", "w") as out_file:
     print(name, file=out_file)
     out_file.close()
 
+with open("name.txt", "r") as in_file:
+    name = in_file.readline()
+    print(f"Your name is {name}")
+    in_file.close()
