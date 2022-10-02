007804e6d99d04846e63dbd71d7973bbc9a78bfb
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-02 15:52:44 +1000

    Modify comments of 2 block of codes in files.py

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,15 +4,15 @@ Files
 This program ask the user for their name, and then it writes it into a file.
 """
 
-# name = input("Name: ").capitalize()
-# with open("name.txt", "w") as out_file:
-#     print(name, file=out_file)
-#     out_file.close()
-#
-# with open("name.txt", "r") as in_file:
-#     name = in_file.readline()
-#     print(f"Your name is {name}")
-#     in_file.close()
+name = input("Name: ").capitalize()
+with open("name.txt", "w") as out_file:
+    print(name, file=out_file)
+    out_file.close()
+
+with open("name.txt", "r") as in_file:
+    name = in_file.readline()
+    print(f"Your name is {name}")
+    in_file.close()
 
 
 with open("numbers.txt", "r") as in_file:
