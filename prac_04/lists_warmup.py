30b2f711c0f995a6023f9a42ac2b87f8ac7eea03
jc785304 <xanderdino.caubat@my.jcu.edu.au>
2022-10-06 15:41:42 +1000

    Added warm-up program

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,18 @@
+"""
+CP1404/CP5632 - Practical 3
+Warm-Up
+This program prints a list of number.
+"""
+
+# this will print the list of numbers
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# 3
+# 2
+# 1
+# 1, 4, 1, 5, 9
+# 1, 5
+# True
+# False
+# False
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
