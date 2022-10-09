336e7b64faa3c18898d40b8bd46a783439965f32
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-09 18:59:18 +1000

    Modify list exercise program to use valid username program

--- prac_04/list_exercise.py
+++ prac_04/list_exercise.py
@@ -29,3 +29,12 @@ def get_numbers(numbers):
 
 
 main()
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted.")
+else:
+    print("Access denied")
