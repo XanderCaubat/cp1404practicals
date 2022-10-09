e8eee729f8cb1b08c42f94e180146f77929c10ed
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-09 20:51:00 +1000

    Modify quick pick program to use get valid number function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -11,7 +11,7 @@ RANDOM_NUMBERS = 6
 
 
 def main():
-    number_of_picks = int(input("How many quick picks? "))
+    number_of_picks = get_valid_number()
     for i in range(number_of_picks):
         picks = []
         for j in range(RANDOM_NUMBERS):
@@ -19,7 +19,16 @@ def main():
             while random_number in picks:
                 random_number = random.randint(MIN, MAX)
             picks.append(random_number)
-        print(random_number, end=' ')
+        picks.sort()
+        print(" ".join([f"{random_number}" for random_number in picks]))
+
+
+def get_valid_number():
+    number_of_picks = int(input("How many quick picks? "))
+    while number_of_picks <= 0:
+        print("Invalid number of picks!")
+        number_of_picks = int(input("How many quick picks? "))
+    return number_of_picks
 
 
 main()
