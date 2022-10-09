7501321b141ae012dbe067db09b54cb9ea8bcd09
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-09 20:35:11 +1000

    Modify quick pick program to use random module and for loop statement

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,4 +3,23 @@ CP1404/CP5632 - Practical 4
 Quick Picks
 This program ask user how many quick picks they wish to generate.
 """
+import random
 
+MIN = 1
+MAX = 45
+RANDOM_NUMBERS = 6
+
+
+def main():
+    number_of_picks = int(input("How many quick picks? "))
+    for i in range(number_of_picks):
+        picks = []
+        for j in range(RANDOM_NUMBERS):
+            random_number = random.randint(MIN, MAX)
+            while random_number in picks:
+                random_number = random.randint(MIN, MAX)
+            picks.append(random_number)
+        print(random_number, end=' ')
+
+
+main()
