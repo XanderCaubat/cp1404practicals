0f4b86752764d4aab7550298908dcbce1a2be480
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-09 18:52:35 +1000

    Add list exercise program

---
+++ prac_04/list_exercise.py
@@ -0,0 +1,31 @@
+"""
+CP1404/CP5632 - Practical 4
+List Exercise
+This program ask the users numbers and display information about them.
+"""
+MAX = 5
+
+
+def main():
+    numbers = []
+    print("Enter 5 numbers.")
+    get_numbers(numbers)
+    display_information(numbers)
+
+
+def display_information(numbers):
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
+
+
+def get_numbers(numbers):
+    for i in range(MAX):
+        number = int(input("Number: "))
+        numbers.append(number)
+    return numbers
+
+
+main()
