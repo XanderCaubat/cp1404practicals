00c6723b5e0cbadf9bf7a454a74daebf49fa4883
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-09 18:20:02 +1000

    Modify the get data function to return a list in subject reader program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -15,16 +15,16 @@ def main():
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    # added empty subject list to pass into
+    subjects = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])
+        # this line will append each parts as a list
+        subjects.append([parts[0], parts[1], parts[2]])
     input_file.close()
+    return subjects
 
 
 main()
