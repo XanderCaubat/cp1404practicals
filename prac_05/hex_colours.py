b20e88d07dc2e2c3fc6142ac09a8e5446252cce3
jc785304 <xanderdino.caubat@my.jcu.edu.au>
2022-10-13 17:09:27 +1000

    Modify hex colours program to use dictionary

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -4,3 +4,6 @@ HEX COLOURS
 This program ask the user to look up a hexadecimal colour.
 """
 
+HEX_TO_COLOURS = {"aliceblue": "f0f8ff", "babypink": "#f4c2c2", "cadetblue": "#5f9ea0", "daffodil": "#ffff31",
+                  "ebony": "555d50", "fawn": "#e5aa70", "gainsboro": "#dcdcdc"}
+
