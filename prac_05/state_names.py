513fc4c02bdb84eec276587a39cfe882fb134fed
jc785304 <xanderdino.caubat@my.jcu.edu.au>
2022-10-13 16:19:52 +1000

    Modify state names program to use try/except instead of if statement

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -10,10 +10,10 @@ print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
+    except KeyError:
+        print("Invalid Key Input!")
     state_code = input("Enter short state: ").upper()
 for state_code in CODE_TO_NAME:
     print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
