28824aac6936e1c15fa2f75aab49a1da7bbe27f3
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-15 20:12:05 +1000

    Modify emails program to use while loop and print statement

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -10,12 +10,18 @@ Actual:    minutes
 def main():
     email_to_names = {}
     email = input("Email: ")
-    name = extract_name(email)
-
     while email != "":
+        name = extract_name(email)
+        confirmation = input(f"Is you name {name}? (Y/N) ").lower()
+        if confirmation == "y" or confirmation == "":
+            email_to_names[email] = name
+        elif confirmation == "n":
+            actual_name = input("Name: ")
+            email_to_names[email] = actual_name
         email = input("Email: ")
-
-    print(name)
+    print(f"{name} ({email})" for email, name in email_to_names.items())
+    # for email, name in email_to_names.items():
+    #     print(f"{name} ({email})")
 
 
 def extract_name(email):
