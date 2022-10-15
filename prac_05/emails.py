46878ec51b22bd528b0071e7730cc42568e1eafb
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-15 19:32:52 +1000

    Modify emails program to use extract name function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,3 +5,24 @@ This program stores user's emails and name in a dictionary.
 Estimate: 100 minutes
 Actual:    minutes
 """
+
+
+def main():
+    email_to_names = {}
+    email = input("Email: ")
+    name = extract_name(email)
+
+    while email != "":
+        email = input("Email: ")
+
+    print(name)
+
+
+def extract_name(email):
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
+
+
+main()
