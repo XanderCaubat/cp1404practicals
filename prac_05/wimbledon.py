76f3e25ed72876a7ee0be37f9830ce7e4bfa1957
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-15 21:14:46 +1000

    Add wimbledon program

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,7 @@
+"""
+CP1404/CP5632 - Practical 5
+Wimbledon
+This program read the wimbledon csv file, process the data and display processed information
+Estimate: 200 minutes
+Actual:   minutes
+"""
