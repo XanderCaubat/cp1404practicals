19281ddff90ba6c1e8492ea8dc9a9d0fc5b7f7e2
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-15 14:03:28 +1000

    Add print statement to word occurrence program

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -9,7 +9,16 @@ Actual:
 word_to_count = {}
 text = input("Text: ")
 words = text.split()
+
+
 for word in words:
     count = word_to_count.get(word, 0)
     word_to_count[word] = count + 1
-print(word_to_count)
+words = sorted(list(word_to_count.keys()))
+print(words)
+word_length = max((len(word) for word in words))
+for word in words:
+    print(f"{word:{word_length}} : {word_to_count[word]}")
+
+
+
