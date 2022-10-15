76d60eb080e3f393da9aef9cbabbc28403c3c091
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-15 13:05:59 +1000

    Modify hex colour program to ask user color input and print the hex number

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -4,6 +4,16 @@ HEX COLOURS
 This program ask the user to look up a hexadecimal colour.
 """
 
-HEX_TO_COLOURS = {"aliceblue": "f0f8ff", "babypink": "#f4c2c2", "cadetblue": "#5f9ea0", "daffodil": "#ffff31",
-                  "ebony": "555d50", "fawn": "#e5aa70", "gainsboro": "#dcdcdc"}
-
+# the dictionary is not constant as the content could change hence it is not in all caps
+colour_to_hexes = {"aliceblue": "f0f8ff", "baby pink": "#f4c2c2", "cadetblue": "#5f9ea0", "daffodil": "#ffff31",
+                   "ebony": "555d50", "fawn": "#e5aa70", "gainsboro": "#dcdcdc", "gray": "#bebebe",
+                   "iceberg": "#71a6d2",
+                   "jade": "00a86b"}
+colour = input("Choose colour? ").lower()
+while colour != "":
+    if colour in colour_to_hexes:
+        print(f"The hex number of {colour} is {colour_to_hexes[colour]}")
+    else:
+        print("Invalid key input!")
+    colour = input("Colour? ").lower()
+print("Thank you for using the Color to Hex program, have a nice day!")
