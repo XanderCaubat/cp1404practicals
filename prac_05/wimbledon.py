8e191a57b452359d6fcfff90ad4675494bf2a517
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-16 15:21:02 +1000

    Modify wimbledon program to use functions

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -6,21 +6,39 @@ Estimate: 200 minutes
 Actual:   minutes
 """
 
+FILENAME = '../prac_05/wimbledon.csv'
+
 
 def main():
-    champion_to_win_count = {}
-    with open('../prac_05/wimbledon.csv', 'r', encoding="utf-8-sig") as in_file:
-        lines = [line.strip() for line in in_file.readlines()]
-        winners = set(lines)
-        champions = [winner for winner in winners]
-        for champion in champions:
-            parts = champion.split(',')
-            name = parts[2]
-            win_count = champion_to_win_count.get(name, 0)
-            champion_to_win_count[name] = win_count + 1
+    get_logs()
+    logs = get_logs()
+    champion_to_win_count = process_logs(logs)
+    print_results(champion_to_win_count)
+
+
+def print_results(champion_to_win_count):
+    print(champion_to_win_count)
     print("Wimbledon champions:")
     for champion in champion_to_win_count:
         print(f"{champion} {champion_to_win_count[champion]}")
+
+
+def process_logs(logs):
+    champion_to_win_count = {}
+    champions = [winner for winner in logs]
+    for champion in champions:
+        parts = champion.split(',')
+        name = parts[2]
+        win_count = champion_to_win_count.get(name, 0)
+        champion_to_win_count[name] = win_count + 1
+    return champion_to_win_count
+
+
+def get_logs():
+    with open(FILENAME, 'r', encoding="utf-8-sig") as in_file:
+        lines = [line.strip() for line in in_file.readlines()]
+        logs = set(lines)
+    return sorted(logs)
 
 
 main()
