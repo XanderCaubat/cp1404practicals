5fd1a6ebef06dad92690dd54fbc458de57443ca8
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-16 14:53:42 +1000

    Modify wimbledon program to use list and dictionary of winners

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,11 +8,19 @@ Actual:   minutes
 
 
 def main():
+    champion_to_win_count = {}
     with open('../prac_05/wimbledon.csv', 'r', encoding="utf-8-sig") as in_file:
         lines = [line.strip() for line in in_file.readlines()]
         winners = set(lines)
-        print(winners)
-        print(len(winners))
+        champions = [winner for winner in winners]
+        for champion in champions:
+            parts = champion.split(',')
+            name = parts[2]
+            win_count = champion_to_win_count.get(name, 0)
+            champion_to_win_count[name] = win_count + 1
+    print("Wimbledon champions:")
+    for champion in champion_to_win_count:
+        print(f"{champion} {champion_to_win_count[champion]}")
 
 
 main()
