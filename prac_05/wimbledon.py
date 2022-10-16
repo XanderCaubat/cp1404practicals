bd302b678fc8fed76299f4aeb26c89698fd89c13
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-16 15:36:46 +1000

    Modify print results function in wimbledon program

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -12,26 +12,30 @@ FILENAME = '../prac_05/wimbledon.csv'
 def main():
     get_logs()
     logs = get_logs()
-    champion_to_win_count = process_logs(logs)
-    print_results(champion_to_win_count)
+    champion_to_win_count, countries = process_logs(logs)
+    print_results(champion_to_win_count, countries)
 
 
-def print_results(champion_to_win_count):
+def print_results(champion_to_win_count, countries):
     print(champion_to_win_count)
     print("Wimbledon champions:")
     for champion in champion_to_win_count:
         print(f"{champion} {champion_to_win_count[champion]}")
+    print(f"This {len(countries)} countries have won Wimbledon:")
+    print(",".join(country for country in sorted(countries)))
 
 
 def process_logs(logs):
     champion_to_win_count = {}
+    countries = set()
     champions = [winner for winner in logs]
     for champion in champions:
         parts = champion.split(',')
         name = parts[2]
+        countries.add(parts[1])
         win_count = champion_to_win_count.get(name, 0)
         champion_to_win_count[name] = win_count + 1
-    return champion_to_win_count
+    return champion_to_win_count, countries
 
 
 def get_logs():
