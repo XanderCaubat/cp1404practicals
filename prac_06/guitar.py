792f2e04c14658db926dd269dc9b2347c0848e89
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-31 20:09:48 +1000

    Add guitar class to prac_06

---
+++ prac_06/guitar.py
@@ -0,0 +1,15 @@
+"""
+CP1404/CP5632 - Practical 6
+Guitar
+This program uses Guitar class as an example
+Estimate: 100 minutes
+Actual:    minutes
+"""
+
+
+class Guitar:
+
+    def __init__(self, name, year, cost):
+        self.name = name
+        self.year = year
+        self.cost = cost
