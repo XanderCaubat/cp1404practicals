ace32c7fb727c12d048feca760dc532eda15a022
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-31 21:13:36 +1000

    Remove add_guitar and get_guitar function in guitars.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -12,27 +12,26 @@ from prac_06.guitar import Guitar
 def main():
     print("My guitars!")
     guitars = []
-    cost, name, year = get_guitar()
-    # add_guitar(cost, guitars, name, year)
-    while name != "":
-        cost, name, year = get_guitar()
-        # add_guitar(cost, guitars, name, year)
-        guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-        guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-    print("These are my guitars:")
-
-
-def add_guitar(cost, guitars, name, year):
-    guitar = [name, year, cost]
-    guitars.append(guitar)
-    print(f"{name} ({year}) : ${cost:.2f} added.")
-
-
-def get_guitar():
     name = input("Name: ")
-    year = int(input("Year: "))
-    cost = float(input("Cost: $"))
-    return cost, name, year
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = [name, year, cost]
+        guitars.append(guitar)
+        display_guitar_added(cost, name, year)
+        name = input("Name: ")
+    print("These are my guitars:")
+    vintage_string = ""
+    for i, guitar in enumerate(guitars, 1):
+        if guitar.is_vintage():
+            vintage_string = "(vintage)"
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+def display_guitar_added(cost, name, year):
+    print(f"{name} ({year}) : ${cost:.2f} added.")
 
 
 main()
