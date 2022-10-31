632d61114d1ca15474ecdc2d276f095c2b23dcdf
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-31 20:33:45 +1000

    Modify print statements in guitar_test.py

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -2,5 +2,8 @@ from prac_06.guitar import Guitar
 
 
 gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
-print(gibson.get_age())
-print(gibson.is_vintage())
+another_guitar = Guitar("Another Guitar", 2013, 700.00)
+print(f"{gibson.name} get_age() - Expected 100. Got {gibson.get_age()}")
+print(f"{another_guitar.name} get_age() - Expected 9. Got {another_guitar.get_age()}")
+print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
