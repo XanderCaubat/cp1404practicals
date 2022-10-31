1ae69bea8bee3be2f48f7c5d688a26b4a8673074
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-10-31 19:22:56 +1000

    Add languages.py and programming_language.py to prac_06

---
+++ prac_06/languages.py
@@ -0,0 +1,7 @@
+"""
+CP1404/CP5632 - Practical 6
+Languages
+Demonstration code when using the Class ProgrammingLanguage
+Estimate: 100 minutes
+Actual:
+"""
---
+++ prac_06/programming_language.py
@@ -0,0 +1,7 @@
+"""
+CP1404/CP5632 - Practical 5
+Programming Languages
+Class for the programming language
+Estimate: 100 minutes
+Actual:
+"""
