5a7ec3122a68f54cbbe40d2dac7cc2b5d20908f1
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-07 21:56:30 +1000

    Modify save_guitars function to write a string on the first line

--- prac_07/guitar_file_reader.py
+++ prac_07/guitar_file_reader.py
@@ -24,8 +24,9 @@ def main():
 
 def save_guitars(guitars):
     with open(FILENAME, 'w') as out_file:
+        out_file.write("Name,Year,Cost\n")
         for guitar in guitars:
-            print(guitar.name, guitar.year, guitar.cost, file=out_file)
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
 
 
 def print_guitars(guitars):
