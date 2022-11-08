78d38b058fa5ef9a723054ce64a4ef979ab6d3bc
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-08 17:52:01 +1000

    Add project.py, project_management.py and projects.txt files to prac_07

---
+++ prac_07/project.py
@@ -0,0 +1,7 @@
+"""
+CP1404/CP5632 - Practical 7
+Project
+project Class
+Estimate: 30 minutes
+Actual:      minutes
+"""
---
+++ prac_07/project_management.py
@@ -0,0 +1,7 @@
+"""
+CP1404/CP5632 - Practical 7
+Project Management Program
+This program manage project priority, completion status and estimated cost
+Estimate: 100 minutes
+Actual:      minutes
+"""
