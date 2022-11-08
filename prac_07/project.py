55cd6c9bb4d48ef78ddb58637273602408b4071c
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-08 17:57:56 +1000

    Add standard methods to project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -5,3 +5,20 @@ project Class
 Estimate: 30 minutes
 Actual:      minutes
 """
+
+
+class Project:
+
+    def __init__(self, name, start_date, priority=0, cost=0.00, percent=0.00):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost = cost
+        self.percent = percent
+
+    def __repr__(self):
+        return f"{self.name}, starting date: {self.start_date}, " \
+               f"priority = {self.priority}, ${self.cost}, {self.percent}%"
+
+    def __lt__(self, other):
+        return self.priority < other.priority
