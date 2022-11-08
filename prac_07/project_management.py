a7e0936b531c7012819229ced9db9cbbb433e5e7
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-08 18:05:22 +1000

    Add main structure to project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,3 +5,32 @@ This program manage project priority, completion status and estimated cost
 Estimate: 100 minutes
 Actual:      minutes
 """
+
+from prac_07.project import Project
+FILENAME = "projects.txt"
+MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new " \
+       "project\n- (U)pdate project\n- (Q)uit "
+
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            pass
+        elif choice == "S":
+            pass
+        elif choice == "D":
+            pass
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        print(MENU)
+        choice = input(">>> ").upper()
+
+
+
+main()
