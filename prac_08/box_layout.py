947cc2d27c9b5c5d0693a75dfbd748e525ca5074
jc785304 <xanderdino.caubat@my.jcu.edu.au>
2022-11-10 16:31:44 +1000

    Modify box_layout kv/py files to use button events

--- prac_08/box_layout.py
+++ prac_08/box_layout.py
@@ -4,3 +4,24 @@ Box Layout App
 Estimate: 30 minutes
 Actual:     minutes
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        print('test')
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
+    def handle_clear(self):
+        self.root.ids.output_label.text = "Hello"
+        self.root.ids.input_name.text = ""
+
+
+BoxLayoutDemo().run()
