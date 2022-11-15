0a994eb2fe3bc69d1e52b3db5d68d9a00bad9056
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-15 14:18:08 +1000

    Add convert miles/km py/kv files

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,18 @@
+"""
+CP1404/CP5632 - Practical 8
+Convert Miles to Kilometers App
+Estimate: 20 minutes
+Actual:     minutes
+"""
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class KmMilesConverterApp(App):
+
+    def build(self):
+        Window.size = (200, 100)
+        self.title = "Miles/Km Converter"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
