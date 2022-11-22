c85d494f824b0dc30e65c6e71d1782e81e880cea
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-22 22:44:44 +1000

    Add docstring comment on __init__ function

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -12,12 +12,13 @@ class SilverServiceTaxi(Taxi):
     flag_fall = 4.50
 
     def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi instance,based in parent class Taxi"""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
 
     def __str__(self):
-        return f"{super().__str__()}, {self.get_fare()} plus flag fall of {self.flag_fall} "
+        return f"{super().__str__()}, ${super().get_fare()} plus flag fall of {self.flag_fall} "
 
     def get_fare(self):
         return self.flag_fall + self.price_per_km
