f24e6486997534355f85e5e005c1edffaab231ae
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-22 22:08:11 +1000

    Add unreliable car method test in unreliable_car_test.py

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -6,3 +6,21 @@ Estimate: 15 minutes
 Actual:    minutes
 """
 
+from prac_09.unreliable_car import UnreliableCar
+
+
+def main():
+    my_unreliable_car = UnreliableCar("Ford 1991", 100, 23)
+    my_new_bought_car = UnreliableCar("Lotus 2022", 100, 90)
+    my_unreliable_car.drive(100)
+    print(my_unreliable_car)
+    for i in range(1, 15):
+        print(f"Distance {i} km")
+        print(f"{my_unreliable_car.name} drove {my_unreliable_car.drive(i)}km")
+        print(f"{my_new_bought_car.name} drove {my_new_bought_car.drive(i)}km")
+
+    print(my_unreliable_car)
+    print(my_new_bought_car)
+
+
+main()
