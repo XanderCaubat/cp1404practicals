4431d247269d107dd22f01b999144b599625ddca
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-22 23:26:36 +1000

    Add band class to band.py

--- prac_09/band.py
+++ prac_09/band.py
@@ -6,3 +6,23 @@ Estimate: 15 minutes
 Actual:    minutes
 """
 
+
+class Band:
+
+    def __init__(self, name=""):
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        return f"{self.name} ({self.musicians})"
+
+    def __repr__(self):
+        return str(vars(self))
+
+    def add(self, musician):
+        self.musicians.append(musician)
+
+    def play(self):
+        if not self.musicians:
+            return f"{self.name} needs an instrument"
+        return f"{self.name} is playing: {self.musicians[0]}"
