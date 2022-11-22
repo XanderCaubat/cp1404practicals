0dfcc80cce9299e3df67e5d031e17e2fa75f76c5
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-22 21:48:49 +1000

    Add drive method in unreliable_car.py

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -5,14 +5,23 @@ Estimate: 15 minutes
 Actual:    minutes
 """
 
+from random import randint
 from prac_09.car import Car
 
 
 class UnreliableCar(Car):
 
-    def __init__(self, name, fuel):
+    def __init__(self, name, fuel, reliability):
         """Initialised UnreliableCar class"""
         super().__init__(name, fuel)
+        self.reliability = reliability
 
     def __str__(self):
         return f"{super().__str__()}"
+
+    def drive(self, distance):
+        random_distance = randint(0, 100)
+        if random_distance >= self.reliability:
+            distance = 0
+        distance_drive = super().drive(distance)
+        return distance_drive
