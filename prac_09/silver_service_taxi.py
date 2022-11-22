b814642b857d40374034c64d513811bb28b229af
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-22 22:29:53 +1000

    Add silver_service_taxi.py in prac_09

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 - Practical 9
+Silver Service taxi class
+Estimate: 15 minutes
+Actual:    minutes
+"""
+
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    flag_fall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        return f"{super().__str__()}, {self.get_fare()} plus flag fall of {self.flag_fall} "
+
+    def get_fare(self):
+        return self.flag_fall + self.price_per_km
