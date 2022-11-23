2d4f7decd68cea4fb70226c0f35e68fd09a32dba
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-23 21:46:30 +1000

    Add display expression in choice menu

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -11,13 +11,18 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 MENU = "Let's drive!\nq)uit, c)hoose, d)rive"
 
+
 def main():
+    current_taxi = None
     print(MENU)
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     choice = input(">>> ").upper()
     while choice != 'Q':
         if choice == 'C':
-            pass
+            for i, taxi in enumerate(taxis):
+                print(f"{i} - {taxi}")
+            number_choice = int(input(">>> "))
+            current_taxi = taxis[number_choice]
         elif choice == 'D':
             pass
         else:
