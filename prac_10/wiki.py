61a3bfa4a05d916d5307ee8c8bb74091122af175
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-29 22:25:45 +1000

    Refactor variable name and add wikipedia page method

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -8,8 +8,10 @@ search_topic = input("Search: ")
 while search_topic != "":
     try:
         topic_summary = wikipedia.summary(f"{search_topic}", sentences=5)
-        suggested_topic = wikipedia.suggest(search_topic, autosuggest=False)
+        topic_page = wikipedia.page(search_topic)
+        print(topic_page.title)
         print(topic_summary)
+        print(topic_page.url)
         search_topic = input("Search: ")
     except wikipedia.exceptions.DisambiguationError as topics:
         print(topics)
