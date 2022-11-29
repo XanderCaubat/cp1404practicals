2b8c721277bf3a05addb7bea955dd491603519d9
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2022-11-29 22:11:56 +1000

    Add wikipedia module to use wikipedia methods

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -2,3 +2,14 @@
 CP1404/CP5632 - Practical 10
 Wikipedia API & Python Library
 """
+import wikipedia
+
+search_topic = input("Search: ")
+while search_topic != "":
+    try:
+        topic_summary = wikipedia.summary(f"{search_topic}", sentences=5)
+        suggested_topic = wikipedia.suggest(search_topic, autosuggest=False)
+        print(topic_summary)
+        search_topic = input("Search: ")
+    except wikipedia.exceptions.DisambiguationError as topics:
+        print(topics)
